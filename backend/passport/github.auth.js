1fdff2cf9e20ee9bc3c1d4cf795fbfccac4c7d74
akshatj22 <akshat22j@gmail.com>
2024-04-13 16:23:49 +0530

    Callback Url updated

--- backend/passport/github.auth.js
+++ backend/passport/github.auth.js
@@ -15,7 +15,7 @@ passport.deserializeUser(function (obj, done) {
 passport.use(new GitHubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: "/api/auth/github/callback"
+    callbackURL: "https://github-clone-2ewc.onrender.com/api/auth/github/callback"
 },
 
   async  function (accessToken, refreshToken, profile, done) {
